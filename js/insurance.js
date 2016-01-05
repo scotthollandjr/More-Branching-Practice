296c867ddd67aafceda3fe5f0b22cb3c218bccb6
Scott Holland <scotthollandjr@gmail.com>
2016-01-05 12:49:25 -0800

    Add insurance project files

--- js/insurance.js
+++ js/insurance.js
@@ -1,1 +1,19 @@
-insurance.js
+$(document).ready(function() {
+  $("form#insurance").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    if (age >= 16) {
+      var quote = (100 - age) * 3;
+      if (gender === 'male' && age < 26) {
+        quote += 50;
+      }
+      $("#rate").empty().append(quote);
+      $("#quote").show();
+    } else {
+      alert("Please enter a valid age");
+    }
+
+    event.preventDefault();
+  });
+});
